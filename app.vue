2386bf6ee4380033a09d84d3be632b6b9b60cf4b
Brian Bronkhorst <brian.bronkhorst@bttr.nl>
2024-12-09 11:59:14 +0100

    Added nuxt cursus repo

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout :name="layout">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<!--<script setup>-->
+<!--const layout = 'another';-->
+<!--</script>-->
+<script setup lang="ts">
+useHead({
+  // as a string,
+  // where `%s` is replaced with the title
+  titleTemplate: '%s - Site Title',
+})
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+  ],
+  colorMode: {
+    classSuffix: ''
+  }
+})
